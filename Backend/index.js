b8c99f0fb6eab1c27d1ab732a452c18a81bf819a
madhuraothota <madhuraothota>
2023-02-20 03:53:26 +0530

    build

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const path = require("path");
 const { connectDb } = require("./db/connectDb");
 const blogRoutes = require("./routes/blogRoutes");
 const cors = require('cors')
@@ -7,14 +8,21 @@ const app = express();
 const PORT = 8001;
 
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({extended:true}));
+
 connectDb();
 
-console.log(process.env.MONGODB_URI);
 
 app.use("/api/blog/",blogRoutes);
 
+
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.get("*", (req,res) =>
+        res.sendFile(path.join(__dirname,"..frontend/build/index.html"))
+);
   
+
+
 app.listen(PORT, () => {
   console.log("Backend is running on port:", PORT);
 });
